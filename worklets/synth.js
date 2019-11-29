0ed2e1027c7b83ef6ade0e550743fbb88460bf0c
TimDaub <Tim.Daubenschuetz@gmail.com>
2019-11-29 14:26:50 +0000

    Deploying to gh-pages from master ff50b2c16afb6ae69762110662d19d19b345e7b9

---
+++ worklets/synth.js
@@ -0,0 +1,50 @@
+//@format
+import Module from "../bundle-wasm.js";
+
+class SynthWorklet extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.kernel = Module();
+    this.voiceManager = new this.kernel.VoiceManager(sampleRate, 64);
+
+    this.port.onmessage = this.handleEvents.bind(this);
+    console.log("Worklet launched successfully");
+  }
+
+  handleEvents({ data }) {
+    if (data.name === "NoteOn") {
+      this.voiceManager.onNoteOn(data.key, 0, 2000, 0.5, 10);
+    } else if (data.name === "NoteOff") {
+      this.voiceManager.onNoteOff(data.key);
+    } else if (data.name === "Envelope") {
+      if (data.key === "xa") {
+        this.voiceManager.setXA(data.value);
+      } else if (data.key === "xd") {
+        this.voiceManager.setXD(data.value);
+      } else if (data.key === "ys") {
+        this.voiceManager.setYS(data.value);
+      } else if (data.key === "xr") {
+        this.voiceManager.setXR(data.value);
+      }
+    }
+  }
+
+  process(inputs, outputs, parameters) {
+    const output = outputs[0];
+    for (
+      let channel = 0, numberOfChannels = output.length;
+      channel < numberOfChannels;
+      channel++
+    ) {
+      const outputChannel = output[channel];
+
+      const sample = this.voiceManager.nextSample(outputChannel.length);
+      for (let i = 0; i < sample.size(); i++) {
+        outputChannel[i] = sample.get(i) * 0.2;
+      }
+    }
+    return true;
+  }
+}
+
+registerProcessor("SynthWorklet", SynthWorklet);
