2b5d0a91e09a968b1be3db9bda8dd322efe37818
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-12-18 16:05:31 +0100

    Implement square wave

--- src/cpp/oscillator.cc
+++ src/cpp/oscillator.cc
@@ -8,7 +8,7 @@
 
 Oscillator::Oscillator() {}
 
-Oscillator::Oscillator(int wave, int sampleRate) {
+Oscillator::Oscillator(WaveForm wave, int sampleRate) {
   this->wave = wave;
   this->sampleRate = sampleRate;
   level = 0;
@@ -21,10 +21,65 @@ vector<Point> Oscillator::NextSample(int key, int iteration, int bufferSize) {
     float x = FrequencyConstant(key) * n / this->sampleRate;
     // TODO: Integrate tuning later, this might be helpful: https://forum.cockos.com/archive/index.php/t-177881.html
     //float tuning = pow(2.0, 1000/ 1200.0);
-    buffer.push_back({x, sin(x) * level});
+    float y = 0.0;
+    switch(wave) {
+      case SIN:
+        y = sin(x);
+        break;
+      case SQUARE_DIGITAL:
+        y = DigitalSquareWave(x);
+        break;
+      case SQUARE_3:
+        y = SinoidSquareWave(x, 3);
+        break;
+      case SQUARE_4:
+        y = SinoidSquareWave(x, 4);
+        break;
+      case SQUARE_6:
+        y = SinoidSquareWave(x, 6);
+        break;
+      case SQUARE_8:
+        y = SinoidSquareWave(x, 8);
+        break;
+      case SQUARE_16:
+        y = SinoidSquareWave(x, 16);
+        break;
+      case SQUARE_32:
+        y = SinoidSquareWave(x, 32);
+        break;
+      case SQUARE_64:
+        y = SinoidSquareWave(x, 64);
+        break;
+      default:
+        // TODO: Throw error here
+        break;
+    }
+    buffer.push_back({x, y * level});
   }
   
   return buffer;
+}
+
+float Oscillator::SinoidSquareWave(float x, int factor) {
+  factor *= 2;
+  float y = 0.0;
+  for (int i = 1; i <= factor; ++i) {
+    if (i % 2 == 1) {
+      y += sin(x * i) / (i * M_PI);
+    }
+  }
+  return y;
+}
+
+float Oscillator::DigitalSquareWave(float x) {
+  // Digital Square Wave
+  float y = sin(x);
+  if (y >= 0) {
+    y = 1;
+  } else {
+    y = -1;
+  }
+  return y;
 }
 
 float Oscillator::FrequencyConstant(int key) {
@@ -40,7 +95,7 @@ void Oscillator::SetLevel(float value) {
 EMSCRIPTEN_BINDINGS(Oscillator) {
   emscripten::class_<Oscillator>("Oscillator")
     .constructor<>()
-    .constructor<int, int>()
+    .constructor<WaveForm, int>()
     .function("nextSample", &Oscillator::NextSample)
     .function("frequencyConstant", &Oscillator::FrequencyConstant)
     .function("setLevel", &Oscillator::SetLevel);
--- src/cpp/oscillator.h
+++ src/cpp/oscillator.h
@@ -7,17 +7,40 @@
 
 using namespace std;
 
+enum WaveForm {
+  SIN = 0,
+  SQUARE_DIGITAL,
+  SQUARE_3,
+  SQUARE_4,
+  SQUARE_6,
+  SQUARE_8,
+  SQUARE_16,
+  SQUARE_32,
+  SQUARE_64,
+  SAW_DIGITAL,
+  SAW_3,
+  SAW_4,
+  SAW_6,
+  SAW_8,
+  SAW_16,
+  SAW_32,
+  SAW_64,
+  TRIANGLE
+};
 
 class Oscillator {
 private:
-  int wave, sampleRate;
+  int sampleRate;
+  WaveForm wave;
   float level;
   vector<Point> buffer;
   friend class VoiceManager;
+  float DigitalSquareWave(float x);
+  float SinoidSquareWave(float x, int factor);
 
 public:
   Oscillator();
-  Oscillator(int wave, int sampleRate);
+  Oscillator(WaveForm wave, int sampleRate);
   vector<Point> NextSample(int key, int iteration, int bufferSize);
   static float FrequencyConstant(int key);
   void SetLevel(float value);
